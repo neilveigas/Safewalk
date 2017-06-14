114844d7f7cd96e2eb5310980e03b27cccd8726d
neilveigas <neilveigas@gmail.com>
2017-06-13 20:08:36 -0400

    Create Requester.java

--- Requester.java
+++ Requester.java
@@ -16,58 +16,6 @@ public class Requester implements MessageListener {
 		this.st = st;
 		st.setMessageListener(this);
 		action();
-		//int i = 0;
-		//    String input = null;
-		//    Scanner s = new Scanner(System.in);
-		//    while (true) {
-		//      if (be == false) {
-		//        menu();
-		//        System.out.printf("Enter your location (1-5): ");
-		//        
-		//        input = s.nextLine();
-		//        
-		//        try 
-		//        {
-		//          int numInput = Integer.parseInt(input);
-		//          //System.out.println("IN TRY");
-		//          if (numInput > 0 && numInput < 6)
-		//          {
-		//            String loc = locationAb(numInput);
-		//            String messg = "REQUEST " + loc;
-		//            
-		//            System.out.printf("Waiting for volunteer...\n");
-		//            be = true;
-		//            try{
-		//              st.sendMessage(messg); 
-		//              //break;
-		//            } catch(ChannelException e){
-		//              e.printStackTrace();
-		//            }
-		////                    try {
-		////                        Thread.sleep(2000);
-		////                    } catch (InterruptedException e) {
-		////                        e.printStackTrace();
-		////                    }
-		//            // recieve id from server, send id and message as parameters to the server recievmessage method
-		//          }
-		//          
-		//          else
-		//          {
-		//            System.out.println("Invalid input. Please try again.");
-		//            
-		//          }
-		//        }
-		//        catch (Exception e) {
-		//          System.out.println("Invalid number format. Please try again.");
-		//        }
-		//        
-		//        
-		//      }
-		//      System.out.printf("");
-		//    }
-		//    
-
-
 	}
 
 	@Override
@@ -75,7 +23,7 @@ public class Requester implements MessageListener {
 		int id = Integer.parseInt(message.substring(10));
 		System.out.printf("Volunteer %d assigned and will arrive shortly.\n" , id);
 		action();
-		// TODO Auto-generated method stub
+		
 
 	}
 
@@ -112,16 +60,7 @@ public class Requester implements MessageListener {
 		return loc; 
 	}
 
-	//    public void sendMessage (String message) throws ChannelException {
-	//        // send a message, since we did not specify a client ID, then the
-	//        // message will be sent to the server.
-	//        try {
-	//            dr.sendMessage(message);
-	//        } catch (ChannelException e) {
-	//            e.printStackTrace();
-	//        }
-	//    }
-	//
+
 	public void action() {
 		String input = null;
 		while (true) {
@@ -148,14 +87,7 @@ public class Requester implements MessageListener {
 						} catch (ChannelException e) {
 							e.printStackTrace();
 						}
-						//	                    try {
-						//	                        Thread.sleep(2000);
-						//	                    } catch (InterruptedException e) {
-						//	                        e.printStackTrace();
-						//	                    }
-						// recieve id from server, send id and message as parameters to the server recievmessage method
-					}
-
+						
 					else
 					{
 						System.out.println("Invalid input. Please try again.");
